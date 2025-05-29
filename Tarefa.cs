57d5ed882505b9ce7d189eb2671c43734ed11c37
RedomXD <2241582@my.ipleiria.pt>
2025-05-29 09:40:28 +0100

    Teste Classes

---
+++ Tarefa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTasks
+{
+    internal class Tarefa
+    {
+        public int Id;
+        public string Descricao;
+        public EstadoAtual EstadoAtual;
+        public DateTime DataPrevistaInicio;
+        public DateTime DataPrevistaFim;
+        public DateTime? DataRealInicio;
+        public DateTime? DataRealFim;
+        public int Ordem;
+        public int ProgramadorId;
+        public Utilizador Programador;
+        public int GestorId;
+        public Utilizador Gestor;
+        public int TipoTarefaId;
+        public TipoTarefa TipoTarefa;
+    }
+}
