f104e8526778c6dee20fc3127ebe9eadd24fdda5
RedomXD <2241582@my.ipleiria.pt>
2025-05-29 09:14:57 +0100

    1st Commit

---
+++ frmGereUtilizadores.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iTasks
+{
+    public partial class frmGereUtilizadores : Form
+    {
+        public frmGereUtilizadores()
+        {
+            InitializeComponent();
+        }
+
+        private void cbDepartamento_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
